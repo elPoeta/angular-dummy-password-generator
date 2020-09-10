c3738b22e0171b13d6aba5814b5c207a49725b1b
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-10 09:26:32 -0300

    create input handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title: string = "Dummy Password Generator!";
+  password: string = "";
+  length: number = 8;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  onButtonClick() {
+    this.generatePassword();
+  }
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onchangeLength(value: string) {
+    if (+value < 8) {
+      value = "8";
+    }
+    this.length = +value;
+  }
+
+  generatePassword(): string {
+    const letters: string = "abcdefghijklmnopqrstuvwxyz";
+    const numbers: string = "1234567890";
+    const symbols: string = "!@#$%^&*()_+~";
+    let validChars: string = "";
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    let passGen: string = "";
+    for (let i = 0; i < this.length; i++) {
+      const index: number = Math.floor(Math.random() * validChars.length);
+      passGen += validChars[index];
+    }
+    return (this.password = passGen);
+  }
 }
